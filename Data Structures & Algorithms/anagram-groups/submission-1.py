1abf5f8e46b5e69eab2353095ee8da8b8e705bbd
Krishna Kant Pathak <kkpathak.2000@gmail.com>
2026-05-21 16:06:29 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return [[""]] 
+        freq = {}
+        for st in strs:
+            key = "".join(sorted(st))
+            if key not in freq:
+                freq[key] = [st]
+            else:
+                freq[key].append(st)
+        return list(freq.values())
